978351d5391dd69897e0ecbcd6b0cc2f784b2132
janethcajara <janethcajara0@gmail.com>
2024-06-11 16:27:13 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="green";
+    document.getElementById("label3").style.color ="red";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+function input1(){
+    document.getElementById("label1").style.color ="red";
+    document.getElementById("label2").style.color ="red";
+    document.getElementById("label3").style.color ="green";
+}
+
